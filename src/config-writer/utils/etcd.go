8e77670d34284fea6e56e2b26e910ff408abedee
DemonZSD <498537806@qq.com>
2019-09-30 17:48:49 +0800

    go home write

--- src/config-writer/utils/etcd.go
+++ src/config-writer/utils/etcd.go
@@ -21,6 +21,8 @@ import (
 	inilog "config-writer/log"
 	"github.com/sirupsen/logrus"
 	"fmt"
+	"config-writer/types"
+	"time"
 )
 
 type EtcdMutex struct {
@@ -34,7 +36,7 @@ type EtcdMutex struct {
 }
 var logger *logrus.Logger
 func (em * EtcdMutex)init() error {
-	var ctx context.Context
+
 	logger = inilog.GetLog()
 	client, err := clientv3.New(em.Conf)
 	if err != nil {
@@ -43,12 +45,69 @@ func (em * EtcdMutex)init() error {
 	}
 	em.txn = clientv3.NewKV(client).Txn(context.TODO())
 	em.lease = clientv3.NewLease(client)
+	//设置租约过期时间
 	leaseResp,err := em.lease.Grant(context.TODO(),em.Ttl )
 	if err != nil {
 		logger.Errorln(fmt.Sprintf("grant lease failed : %v", err))
 	}
 	ctx, em.cancel = context.WithCancel(context.TODO())
 	em.leaseId = leaseResp.ID
-	_, err = em.lease.KeepAlive(ctx, em.leaseId)
+
 	return err
+}
+
+func DoTask(allocateIP func(_ *types.HostLocal) error, em EtcdMutex, client *clientv3.Client){
+	//启动一个协程去监听
+	leaseRespChan, err := em.lease.KeepAlive(ctx, em.leaseId)  //自动续租
+	go listenLeaseChan(leaseRespChan)
+	//业务处理
+	kv := clientv3.NewKV(client)
+	//创建事务
+	txn := kv.Txn(context.TODO())
+	txn.If(clientv3.Compare(clientv3.CreateRevision("/cron/lock/job9"),"=",0)).
+		Then(clientv3.OpPut("/cron/lock/job9","xxx",clientv3.WithLease(leaseId))).
+		Else(clientv3.OpGet("/cron/lock/job9"))//否则抢锁失败
+
+	//提交事务
+	if txtResp,err :=txn.Commit();err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		//判断是否抢锁
+		if !txtResp.Succeeded {
+			fmt.Println("锁被占用：",string(txtResp.Responses[0].GetResponseRange().Kvs[0].Value))
+			return
+		}
+	}
+
+	fmt.Println("处理任务")
+
+
+
+	//释放锁（停止续租，终止租约）
+	defer cancleFunc()//函数退出取消自动续租
+	defer lease.Revoke(context.TODO(),leaseId) //终止租约（去掉过期时间）
+
+	time.Sleep(10 * time.Second)
+}
+
+
+func listenLeaseChan(leaseRespChan <-chan *clientv3.LeaseKeepAliveResponse) {
+	var (
+		leaseKeepResp *clientv3.LeaseKeepAliveResponse
+	)
+	for {
+		select {
+		case leaseKeepResp = <-leaseRespChan:
+			if leaseKeepResp == nil {
+				fmt.Println("租约失效了")
+				goto END
+			} else {
+				fmt.Println(leaseKeepResp.ID)
+
+			}
+
+		}
+	}
+END:
 }
